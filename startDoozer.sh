282919121b7ee3a431d6d4dd29cb0b242d3b92a3
sclasen <sclasen@salesforce.com>
2011-05-16 17:50:21 -0700

    update start for 0.7

--- startDoozer.sh
+++ startDoozer.sh
@@ -1,4 +1,4 @@
-rm ./doozer-0.6/dooz.log
+rm ./doozer-0.7/dooz.log
 
 set -e
 trap quit INT TERM EXIT
@@ -9,16 +9,18 @@ quit() {
     exit
 }
 
-touch ./doozer-0.6/dooz.log
+export DOOZER_RWSECRET=secret
 
-./doozer-0.6/doozerd -l="127.0.0.1:8046" -w=":8080" >> ./doozer-0.6/dooz.log  2>&1 &
-./doozer-0.6/doozerd -l="127.0.0.1:8047" -a="127.0.0.1:8046" -w=":8081" >> ./doozer-0.6/dooz.log  2>&1 &
-./doozer-0.6/doozerd -l="127.0.0.1:8048" -a="127.0.0.1:8046" -w=":8082" >> ./doozer-0.6/dooz.log  2>&1 &
+touch ./doozer-0.7/dooz.log
+
+./doozer-0.7/doozerd -l="127.0.0.1:8046" -w=":8080" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozer-0.7/doozerd -l="127.0.0.1:8047" -a="127.0.0.1:8046" -w=":8081" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozer-0.7/doozerd -l="127.0.0.1:8048" -a="127.0.0.1:8046" -w=":8082" >> ./doozer-0.7/dooz.log  2>&1 &
 
 sleep 1
 
-printf '' | ./doozer-0.6/doozer -a 127.0.0.1:8046 set /ctl/cal/1 0  > /dev/null
-printf '' | ./doozer-0.6/doozer -a 127.0.0.1:8046 set /ctl/cal/2 0  > /dev/null
+printf '' | ./doozer-0.7/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/1 0  > /dev/null
+printf '' | ./doozer-0.7/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/2 0  > /dev/null
 
 
 trap - INT TERM EXIT
