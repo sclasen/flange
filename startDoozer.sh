ff09c6fcc73b0344b67d1cad974ef27edaad2aab
sclasen <sclasen@salesforce.com>
2011-06-05 17:27:01 -0700

    add osx and linux dists of doozer and tweak start script to use the right one

--- startDoozer.sh
+++ startDoozer.sh
@@ -13,14 +13,20 @@ export DOOZER_RWSECRET=secret
 
 touch ./doozer-0.7/dooz.log
 
-./doozer-0.7/doozerd -l="127.0.0.1:8046" -w=":8080" >> ./doozer-0.7/dooz.log  2>&1 &
-./doozer-0.7/doozerd -l="127.0.0.1:8047" -a="127.0.0.1:8046" -w=":8081" >> ./doozer-0.7/dooz.log  2>&1 &
-./doozer-0.7/doozerd -l="127.0.0.1:8048" -a="127.0.0.1:8046" -w=":8082" >> ./doozer-0.7/dooz.log  2>&1 &
+OS=linux
+UNAME=`uname`
+if [ "$UNAME" =  "Darwin" ]; then
+  OS=osx
+fi
+
+./doozer-0.7/$OS/doozerd -l="127.0.0.1:8046" -w=":8080" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozer-0.7/$OS/doozerd -l="127.0.0.1:8047" -a="127.0.0.1:8046" -w=":8081" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozer-0.7/$OS/doozerd -l="127.0.0.1:8048" -a="127.0.0.1:8046" -w=":8082" >> ./doozer-0.7/dooz.log  2>&1 &
 
 sleep 1
 
-printf '' | ./doozer-0.7/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/1 0  > /dev/null
-printf '' | ./doozer-0.7/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/2 0  > /dev/null
+printf '' | ./doozer-0.7/$OS/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/1 0  > /dev/null
+printf '' | ./doozer-0.7/$OS/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/2 0  > /dev/null
 
 
 trap - INT TERM EXIT
