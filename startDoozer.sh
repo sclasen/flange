51387daf1c21ec191f2534388a8dd2b5327bc9f4
sclasen <sclasen@salesforce.com>
2011-09-13 10:51:54 -0700

    bump to doozer 0.8

--- startDoozer.sh
+++ startDoozer.sh
@@ -19,14 +19,14 @@ if [ "$UNAME" =  "Darwin" ]; then
   OS=osx
 fi
 
-./doozer-0.7/$OS/doozerd -l="127.0.0.1:8046" -w=":8080" >> ./doozer-0.7/dooz.log  2>&1 &
-./doozer-0.7/$OS/doozerd -l="127.0.0.1:8047" -a="127.0.0.1:8046" -w=":8081" >> ./doozer-0.7/dooz.log  2>&1 &
-./doozer-0.7/$OS/doozerd -l="127.0.0.1:8048" -a="127.0.0.1:8046" -w=":8082" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozerd-0.8/$OS-386/doozerd -l="127.0.0.1:8046" -w=":8080" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozerd-0.8/$OS-386/doozerd -l="127.0.0.1:8047" -a="127.0.0.1:8046" -w=":8081" >> ./doozer-0.7/dooz.log  2>&1 &
+./doozerd-0.8/$OS-386/doozerd -l="127.0.0.1:8048" -a="127.0.0.1:8046" -w=":8082" >> ./doozer-0.7/dooz.log  2>&1 &
 
 sleep 1
 
-printf '' | ./doozer-0.7/$OS/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/1 0  > /dev/null
-printf '' | ./doozer-0.7/$OS/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/2 0  > /dev/null
+printf '' | ./doozerd-0.8/$OS-386/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/1 0  > /dev/null
+printf '' | ./doozerd-0.8/$OS-386/doozer -a "doozer:?ca=127.0.0.1:8046&sk=$DOOZER_RWSECRET" set /ctl/cal/2 0  > /dev/null
 
 
 trap - INT TERM EXIT
